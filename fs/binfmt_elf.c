02c959a402a4f9972deca91f358104459f654373
alexax66 <alexax66@mail.ru>
2017-06-22 18:45:22 +0000

    binfmt_elf: use get_random_long()
    Replace calls to get_random_int() followed by a cast to (unsigned long)
    with calls to get_random_long().
    
    Signed-off-by: Daniel Cashman <dcashman@android.com>
    
    Based on patch: https://lkml.org/lkml/2016/2/4/833

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -556,7 +556,8 @@ static unsigned long randomize_stack_top(unsigned long stack_top)
 
 	if ((current->flags & PF_RANDOMIZE) &&
 		!(current->personality & ADDR_NO_RANDOMIZE)) {
-		random_variable = get_random_int() & STACK_RND_MASK;
+		random_variable = get_random_long();
+		random_variable &= STACK_RND_MASK;
 		random_variable <<= PAGE_SHIFT;
 	}
 #ifdef CONFIG_STACK_GROWSUP
