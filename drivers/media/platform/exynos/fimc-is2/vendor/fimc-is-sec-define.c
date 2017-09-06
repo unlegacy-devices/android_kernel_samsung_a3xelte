bcba1b761e6c515b92cbfa0c13771728f12beef9
alexax66 <alexax66@mail.ru>
2017-09-06 18:32:20 +0000

    :qa media: merge with Nougat source

--- drivers/media/platform/exynos/fimc-is2/vendor/fimc-is-sec-define.c
+++ drivers/media/platform/exynos/fimc-is2/vendor/fimc-is-sec-define.c
@@ -17,6 +17,9 @@
 #if defined(CONFIG_CAMERA_EEPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_EEPROM_SUPPORT_FRONT)
 #include <linux/i2c.h>
 #include "fimc-is-device-eeprom.h"
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+#include "fimc-is-device-sensor.h"
+#endif
 #endif
 bool crc32_fw_check = true;
 bool crc32_setfile_check = true;
@@ -1077,6 +1080,9 @@ int fimc_is_sec_rom_power_on(struct fimc_is_core *core, int position)
 	struct fimc_is_module_enum *module = NULL;
 	int sensor_id = 0;
 	int i = 0;
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+	struct fimc_is_device_sensor *device_sensor;
+#endif
 
 	info("%s: Sensor position = %d.", __func__, position);
 
@@ -1087,8 +1093,12 @@ int fimc_is_sec_rom_power_on(struct fimc_is_core *core, int position)
 
 	for (i = 0; i < FIMC_IS_SENSOR_COUNT; i++) {
 		fimc_is_search_sensor_module(&core->sensor[i], sensor_id, &module);
-		if (module)
+		if (module) {
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+			device_sensor = &core->sensor[i];
+#endif
 			break;
+		}
 	}
 
 	if (!module) {
@@ -1104,6 +1114,14 @@ int fimc_is_sec_rom_power_on(struct fimc_is_core *core, int position)
 		ret = -EINVAL;
 		goto p_err;
 	}
+
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+	ret = fimc_is_sensor_mclk_on(device_sensor, GPIO_SCENARIO_ON, module_pdata->mclk_ch);
+	if (ret) {
+		err("mclk_on is fail(%d)", ret);
+		goto p_err;
+	}
+#endif
 
 	ret = module_pdata->gpio_cfg(module->pdev, SENSOR_SCENARIO_READ_ROM, GPIO_SCENARIO_ON);
 	if (ret) {
@@ -1122,6 +1140,9 @@ int fimc_is_sec_rom_power_off(struct fimc_is_core *core, int position)
 	struct fimc_is_module_enum *module = NULL;
 	int sensor_id = 0;
 	int i = 0;
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+	struct fimc_is_device_sensor *device_sensor;
+#endif
 
 	info("%s: Sensor position = %d.", __func__, position);
 
@@ -1132,8 +1153,12 @@ int fimc_is_sec_rom_power_off(struct fimc_is_core *core, int position)
 
 	for (i = 0; i < FIMC_IS_SENSOR_COUNT; i++) {
 		fimc_is_search_sensor_module(&core->sensor[i], sensor_id, &module);
-		if (module)
+		if (module) {
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+			device_sensor = &core->sensor[i];
+#endif
 			break;
+		};
 	}
 
 	if (!module) {
@@ -1155,6 +1180,13 @@ int fimc_is_sec_rom_power_off(struct fimc_is_core *core, int position)
 		err("gpio_cfg is fail(%d)", ret);
 		goto p_err;
 	}
+#if defined(CONFIG_CAMERA_OTPROM_SUPPORT_REAR) || defined(CONFIG_CAMERA_OTPROM_SUPPORT_FRONT)
+	ret = fimc_is_sensor_mclk_off(device_sensor, GPIO_SCENARIO_OFF, module_pdata->mclk_ch);
+	if (ret) {
+		err("mclk_on is fail(%d)", ret);
+		goto p_err;
+	}
+#endif
 
 p_err:
 	return ret;
